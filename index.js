9a7d26f7923e13456675637763b3d73486ae00e7
drdynscript <drdynscript@gmail.com>
2017-03-19 09:53:00 +0100

    Update Hook

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const bodyParser = require('body-parser');
 const restService = express();
 restService.use(bodyParser.json());
 
+restService.post('/echo', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
+
 restService.post('/hook', function (req, res) {
 
     console.log('hook request');
